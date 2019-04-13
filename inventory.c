5174a0a5bd7a9b612871392417522b1f0268b78f
1801174 <e1801174@shell.vamk.fi>
2019-04-13 15:10:14 +0300

    lisätty koodi

---
+++ inventory.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+struct products
+{
+	char product_name[20];
+	int price;
+	int barcode;
+};
+
+void ask_product(struct product table[]);
+void print_products(struct product table[]);
+
+
+int main(void) {
+
+	struct products product[100] = {0};
+
+
+	void ask_product(struct product table[]);
+	void print_products(struct product table[]);
+
+	getche();
+}
+void ask_product(struct product table[]) {
+
+	for (int i = 0; i < 3; i++) {
+		int product;
+		table[1] = product;
+		printf("give me product");
+		scan_s("%", &product);
+		
+	}
+}
