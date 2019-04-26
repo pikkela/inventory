c31bba3b223f04cfd6e9548d99443d02eabba7b8
1801174 <e1801174@shell.vamk.fi>
2019-04-26 23:27:13 +0300

    templatessa

--- inventory.c
+++ inventory.c
@@ -1,10 +1,48 @@
+/***************************************************************************
+ * Copyright (C) $YEAR$ by $AUTHOR$ *
+ * $EMAIL$ *
+ * *
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2 of the License, or *
+ * (at your option) any later version.  *
+ * *
+ * This program is distributed in the hope that it will be useful, *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the *
+ * GNU General Public License for more details.  *
+ * *
+ * You should have received a copy of the GNU General Public License *
+ * along with this program; if not, write to the *
+ * Free Software Foundation, Inc., *
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  *
+ ***************************************************************************/
+ /*********************************************************************
+ *
+ * 1.  NAME
+ * $Source$
+ * 2.  DESCRIPTION
+ *
+ * 6.  VERSIONS
+ * Original:
+ * $Date$ / $Author$
+ *
+ * Version history:
+ * $Log$
+ *
+ **********************************************************************/
+ /*-------------------------------------------------------------------*
+ * HEADER FILES *
+ *--------------------------------------------------------------------*/
 #include <stdio.h>
-
-
+ /*-------------------------------------------------------------------*
+ * GLOBAL VARIABLES *
+ *--------------------------------------------------------------------*/
+ /* Control flags */
+#define DEBUG
+/* Globaal constants */
 #define MAX_PRODUCTS 501
-
-
-
+/* Globaal variables */ /* Globaal structures */
 struct products
 {
 	int product_number;
@@ -13,21 +51,31 @@ struct products
 	float price;
 	int barcode;
 };
+/*-------------------------------------------------------------------* * 
+FUNCTION PROTOTYPES * 
+*--------------------------------------------------------------------*/ void 
+print_products(struct products product[]); void ask_product(struct products 
+product[]); 
+/********************************************************************* * MAIN 
+PROGRAM * 
+**********************************************************************/ int 
+main(void) {
+	struct products product[MAX_PRODUCTS] = { 0 }; /*luodaan structi taulukot*/
 
-void print_products(struct products product[]);
-void ask_product(struct products product[]);
+	ask_product(product);
 
-
-int main(void) {
-
-	struct products product[MAX_PRODUCTS] = {0};
-
-		ask_product(product);
-
-		print_products(product);
-}
-
-
+	print_products(product);
+} /* end of main */
+/********************************************************************* * 
+FUNCTIONS * 
+**********************************************************************/ 
+/********************************************************************* ; F U N 
+C T I O N D E S C R I P T I O N 
+;--------------------------------------------------------------------- ; 
+NAME:ask_product() ; DESCRIPTION: kysyy tuotteet ja laittaa ne listalle ; 
+Input:struct products product[] ; Output: ; Used global variables: ; REMARKS 
+when using this function: 
+;*********************************************************************/
 void ask_product(struct products product[])
 {
 	int ok;
@@ -41,16 +89,16 @@ void ask_product(struct products product[])
 
 		printf("give me products number: ");
 		fgets(trash, 20, stdin);
-		ok = sscanf(trash, "%d", &pronum);
+		ok = sscanf(trash, "%d", &pronum);		/*tarkistetaan onko syöte int tyyppiä ja laitetaan se pronumin arvoksi*/
 
 
 
 		while (ok != 1)
-			{
-				printf("Please use numbers and give me a new number\n");
-				fgets(trash, 20, stdin);
-				ok = sscanf(trash, "%d", &pronum);
-			}
+		{
+			printf("Please use numbers and give me a new number\n");
+			fgets(trash, 20, stdin);
+			ok = sscanf(trash, "%d", &pronum);
+		}
 
 		product[pronum].product_number = pronum;
 
@@ -63,11 +111,11 @@ void ask_product(struct products product[])
 
 
 		while (ok != 1)
-			{
-				printf("Please use numbers and give me a new number\n");
-				fgets(trash, 20, stdin);
-				ok = sscanf(trash, "%d", &product[pronum].amount);
-			}
+		{
+			printf("Please use numbers and give me a new number\n");
+			fgets(trash, 20, stdin);
+			ok = sscanf(trash, "%d", &product[pronum].amount);
+		}
 
 
 		printf("give me products name: ");
@@ -78,48 +126,52 @@ void ask_product(struct products product[])
 		ok = sscanf(trash, "%f", &product[pronum].price);
 
 		while (ok != 1)
-			{
-				printf("Please use numbers and give me a new price\n");
-				fgets(trash, 20, stdin);
-				ok = sscanf(trash, "%f", &product[pronum].price);
-			}
+		{
+			printf("Please use numbers and give me a new price\n");
+			fgets(trash, 20, stdin);
+			ok = sscanf(trash, "%f", &product[pronum].price);
+		}
 
 		printf("give me products barcode: ");
-		fgets(trash, 20, stdin );
+		fgets(trash, 20, stdin);
 		ok = sscanf(trash, "%d", &product[pronum].barcode);
 
 		while (ok != 1)
-			{
-				printf("Please use numbers and give me a new barcode\n");
-				fgets(trash, 20, stdin);
-				ok = sscanf(trash, "%d", &product[pronum].barcode);
-			}
+		{
+			printf("Please use numbers and give me a new barcode\n");
+			fgets(trash, 20, stdin);
+			ok = sscanf(trash, "%d", &product[pronum].barcode);
+		}
 
-		prods++;
+		prods++;		/*lisätään sytettyjen tuotteiden määrä puskuriin ... Mutta sehän ei olekaan pätevä enää tässä systeemissä :( */
 
 		printf("press 1 to exit or press enter to give a next product\n");
 		fgets(trash, 20, stdin);
 		sscanf(trash, "%d", &exit);
 
 
-		if (prods == MAX_PRODUCTS)
-			{
-				exit = 1;
-			}
+		if (prods == MAX_PRODUCTS) /*jos prods on yhtä paljon kun MAX_PRODUCT poistutaan functiosta */
+		{
+			exit = 1;
+		}
 	}
 }
-	void print_products(struct products product[]) 
+/********************************************************************* ; F U N 
+C T I O N D E S C R I P T I O N 
+;--------------------------------------------------------------------- ; 
+NAME:print_products() ; DESCRIPTION: tulostaa tuotelistan ; Input: struct 
+products product[] ; Output: ; Used global variables: ; REMARKS when using this 
+function: 
+;*********************************************************************/
+void print_products(struct products product[])
+{
+	for (int i = 1; i < MAX_PRODUCTS; i++)
+
+	{
+		if (product[i].product_number != 0)	/*tarkistetaan onko tuotenumeron kohdalle syötetty tuote ja tulostetaan jos on*/
 		{
-
-		for (int i = 1; i < MAX_PRODUCTS; i++)
-
-			{
-				if(product[i].product_number != 0)
-				{
-					printf("Product number #%d\n   Amount of product: %d\n   Name of product: %s\n   Price of product: %.3f\n   Barcode of product: %d\n", product[i].product_number, product[i].amount, product[i].product_name, product[i].price, product[i].barcode);
-					printf("\n");
-				}
-			}
+			printf("Product number #%d\n   Amount of product: %d\n   Name of product: %s\n   Price of product: %.3f\n   Barcode of product: %d\n", product[i].product_number, product[i].amount, product[i].product_name, product[i].price, product[i].barcode);
+			printf("\n");
 		}
-
-
+	}
+}
